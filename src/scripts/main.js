86515936dbcd5915e6af14225122a79dff2129c0
Bayer.py <ga.bayer2.0@gmail.com>
2024-06-10 20:03:25 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,31 @@
-console.log("Olá Mundo");
+AOS.init();
+
+const dataDoEvento = new Date("Dec 12, 2024 19:00:00");
+const timeStampDoEvento= dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function(){
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horaEmMs = 1000 * 60 * 60;
+    const minutoEmMs = 1000 * 60;
+
+    //Dia
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    //horas
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horaEmMs);
+    //minutos
+    const minutosAteOEvento = Math.floor((diasAteOEvento % horaEmMs) / minutoEmMs);
+    //segundos
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutoEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = 'Evento Expirado';
+    }
+}, 1000);
